04acf96870fc155421e766c53aa6818a543d0bd2
Yukichi Kawada <yukichikawada@gmail.com>
2018-06-06 14:42:22 -0700

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# web_guesser
+
+Guess the number from 0-99.
+* [jumpstartlabs](http://tutorials.jumpstartlab.com/projects/web_guesser.html) sinatra tutorial
--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,35 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-num = rand(100)
+SECRET_NUM = rand(100)
+
+def validate_guess(params)
+  num = ""
+  if params.nil?
+    message = ""
+  else
+    message = check_guess(params.to_i)
+    num = SECRET_NUM
+  end
+  [message, num]
+end
+
+def check_guess(guess)
+  if SECRET_NUM < guess
+    "Too high!"
+  elsif SECRET_NUM > guess
+    "Too low!"
+  else
+    "Great guess!"
+  end
+end
 
 get '/' do
-  "The SECRET NUMBER is #{num}"
+  num = ""
+  guess = params['guess']
+  message, num = validate_guess(guess)
+  erb :index, :locals => {
+    :num => num,
+    :message => message
+  }
 end
